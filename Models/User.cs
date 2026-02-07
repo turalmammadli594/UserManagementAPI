cfcb42451edf6a221ec11ad815d788dab60566c1
Tural Mammadli <turalmammadli594@gmail.com>
2026-02-07 22:26:50 +0400

    Add User model with basic properties

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace UserManagementAPI.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Department { get; set; } = "";
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+}
